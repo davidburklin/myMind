6b4006fdd8d3b80782f662bcf0c9723dab251119
davidburklin <davidburklin@gmail.com>
2026-08-23 00:49:13 -0500

    Initial commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <UHeader>
+      <template #title>
+        <NuxtImg
+          src="/nuxt-starter-logo.png"
+          alt="Your Logo"
+          height="48"
+          class="my-1"
+        />
+      </template>
+
+      <template #right>
+        <UColorModeButton />
+      </template>
+    </UHeader>
+
+    <UMain>
+      <slot />
+    </UMain>
+
+    <UFooter />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+
+  css: ['~/assets/css/main.css'],
+
+  devtools: { enabled: true },
+
+  image: {
+    dir: 'assets/images',
+  },
+
+  modules: [
+    '@nuxt/devtools',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/ui',
+    '@pinia/nuxt'
+  ]
+})
